159486febbf6336c12de2c958a0db22b559a2800
ak-m <ak.ghub.m@gmail.com>
2020-07-26 12:12:09 -0400

    step1: Initial angular10 integration with jsonplaceholder rest api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentsListComponent } from './comments/comments-list/comments-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentsListComponent
   ],
   imports: [
     BrowserModule,
