428e8a1a204849856c59a3108b6cab8b9db87e95
ak-m <ak.ghub.m@gmail.com>
2020-07-26 13:06:31 -0400

    step2: Implement Get All Comments & Delete Comment functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommentsListComponent } from './comments/comments-list/comments-list.component';
+import { EditCommentComponent } from './comments/edit-comment/edit-comment.component';
+import { AddCommentComponent } from './comments/add-comment/add-comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentsListComponent
+    CommentsListComponent,
+    EditCommentComponent,
+    AddCommentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/comments/comments-list/comments-list.component.ts
+++ src/app/comments/comments-list/comments-list.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import {Comment} from '../Comment';
+import { CommentsService } from '../../services/comments.service';
 
 @Component({
   selector: 'app-comments-list',
@@ -12,13 +13,44 @@ export class CommentsListComponent implements OnInit {
   title:string = "CommentsListComponent";
   comments: Comment[];
 
-  constructor() {
+  constructor(private _commentsService:CommentsService) {
     console.log("CommentsListComponent.constructor called");
    }
 
   ngOnInit(): void {
     console.log("CommentsListComponent.ngOnInit called");
-    this.getMockComments();
+    //this.getMockComments();
+    this.getRealComments();
+  }
+
+  editComment(comment2edit:Comment): void {
+    console.log(`CommentsListComponent.editComment called ${JSON.stringify(comment2edit)}`);
+
+  }
+
+  deleteComment(comment2delete:Comment): void {
+    console.log(`CommentsListComponent.deleteComment called ${comment2delete.id}`);
+    this.comments = this.comments.filter(c => (c.id != comment2delete.id));
+    console.log(`DETELE-FROM-UI: After deleting comments in UI size: ${this.comments.length}`);
+    
+    this._commentsService.deleteComment(920192)
+                      .subscribe((response) => {
+                        console.log(`DETELE-FROM-BACKEND: delete response from sever ${JSON.stringify(response)}`);
+                       }, (error) => {
+                         console.error(`DETELE-FROM-BACKEND: Got error while deleting comments ${error.message}`);
+                       })
+  }
+
+  getRealComments(): void {
+    console.log("CommentsListComponent.getRealComments called");
+
+   this._commentsService.getAllComments()
+                      .subscribe((data) => {
+                        console.log(`got comments from service ${data.length}`);
+                        this.comments = data;
+                      }, (error) => {
+                        console.error(`Got error while getting comments ${error.message}`);
+                      })
   }
 
   getMockComments(): void {
